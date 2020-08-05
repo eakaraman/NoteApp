77985a3212aa03f220ab66fd9289982a3a4445be
eakaraman <eakaraman1@gmail.com>
2020-08-05 17:14:42 +0300

    added EditScreen

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
+import EditScreen from './src/screens/EditScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Edit: EditScreen,
   },
   {
     initialRouteName: 'Home',
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigaton }) => {
   return (
     <View>
       <Text>Home</Text>
+      <Button title="edit" onPress={() => navigaton.navigate('edit')} />
     </View>
   );
 };
